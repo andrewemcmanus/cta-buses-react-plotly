eb945cd2bb65c1fe918afe3f4878069dd0836eb3
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-08-29 17:30:46 -0400

    SVG icons?

--- src/components/ParamSlider.js
+++ src/components/ParamSlider.js
@@ -4,11 +4,11 @@ import { useState } from "react";
 import Plots from "./Plots";
 
 export default function ParamSlider({ boardings, alightings, latitudes, longitudes }) {
-    const [ scale, setScale ] = useState(0.02);
+    const [ scale, setScale ] = useState(0.01);
     return (
         <div>
-            <Slider min={1} max={10} onChange={(e) => {
-                setScale( e / 50 );
+            <Slider min={1} max={20} onChange={(e) => {
+                setScale( e / 100 );
             }} />
             <Plots boardings={boardings.map((i) => (i * scale))} alightings={alightings.map((i) => (i * scale))} latitudes={latitudes} longitudes={longitudes} />
         </div>
